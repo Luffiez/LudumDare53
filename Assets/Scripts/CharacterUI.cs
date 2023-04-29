c8de44c183d914f2d3611885fb16561251dc7998
Erik Rodriguez <Luffiez123@gmail.com>
2023-04-29 16:00:30 +0200

    Charater in queue with wobble and stretch

--- Assets/Scripts/CharacterUI.cs
+++ Assets/Scripts/CharacterUI.cs
@@ -5,6 +5,17 @@ using UnityEngine.UI;
 
 public class CharacterUI : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 2f;
+
+    [Header("Wobble Settings")]
+    [SerializeField] float wobbleSpeed = 2f;
+    [SerializeField] float wobbleStrength = 1f;
+
+    [Header("Squish Settings")]
+    [SerializeField] float squishSpeed = 2f;
+    [SerializeField] float squishStrength = 1f;
+
+    [Header("Sprite Images")]
     [SerializeField] Image head;
     [SerializeField] Image hair;
     [SerializeField] Image ears;
@@ -16,20 +27,51 @@ public class CharacterUI : MonoBehaviour
     [SerializeField] Image facialHair;
     [SerializeField] Image body;
 
+    [HideInInspector] public Transform targetPoint;
+    [HideInInspector] public bool destroyOnReachedTarget;
     Character character;
 
     SpriteManager spriteManager;
+
+    float wobbleCycle = 0;
+    float squishCycle;
 
     private void Start()
     {
         spriteManager = SpriteManager.instance;
         GenerateRandomNew();
+
+        squishCycle = Random.Range(0, 100);
+        squishSpeed *= Random.Range(0.9f, 1.1f);
+
+        wobbleCycle = Random.Range(0, 100);
+        wobbleSpeed *= Random.Range(0.9f, 1.1f);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            GenerateRandomNew();
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //    GenerateRandomNew();
+
+        if (targetPoint != null && Vector2.Distance(transform.position, targetPoint.position) > 0.1f)
+        {
+            transform.localScale = Vector3.one;
+            wobbleCycle += Time.deltaTime * wobbleSpeed;
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(wobbleCycle) * wobbleStrength);
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPoint.position, Time.deltaTime * moveSpeed);
+        }
+        else if (destroyOnReachedTarget)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            squishCycle += Time.deltaTime * squishSpeed;
+            float sin = Mathf.Sin(squishCycle) * squishStrength;
+            transform.localScale = new Vector3(1+ sin, 1- sin, 1); // TODO
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 
     void GenerateRandomNew()
