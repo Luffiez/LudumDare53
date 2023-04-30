15739fa3e7aaf484cd7edc3a5582105829c2aa14
Erik Rodriguez <Luffiez123@gmail.com>
2023-04-30 15:42:24 +0200

    fixed error with 6th char

--- Assets/IDCardUI.cs
+++ Assets/IDCardUI.cs
@@ -30,10 +30,18 @@ public class IDCardUI : MonoBehaviour
 
         cardPreviewText.text = "<b>Name</b>\n";
         cardPreviewText.text += character.GivenName + " " + character.SurName;
+
         cardPreviewText.text += "\n\n<b>Date of Birth</b>\n";
+        cardPreviewText.text += $"{character.BirthDay} / {GetMonthAbbr(character.BirthMonth)} / {character.BirthYear}";
+
+        cardPreviewText.text += "\n\n<b>Personal ID</b>\n";
         cardPreviewText.text += character.PersonIdString;
+
         cardPreviewText.text += "\n\n<b>Sex</b>\n";
         cardPreviewText.text += character.Sex.ToString();
+
+        cardPreviewText.text += "\n\n<b>Expiration Date</b>\n";
+        cardPreviewText.text += character.ExpireMonth + " / " + character.ExpireYear;
 
         cardPreviewText.transform.parent.gameObject.SetActive(true);
     }
@@ -41,6 +49,27 @@ public class IDCardUI : MonoBehaviour
     public void StopPreviewingCard()
     {
         cardPreviewText.transform.parent.gameObject.SetActive(false);
+    }
+
+    string GetMonthAbbr(int monthNumber)
+    {
+        switch (monthNumber)
+        {
+            case 1: return "JAN";
+            case 2: return "FEB";
+            case 3: return "MAR";
+            case 4: return "APR";
+            case 5: return "MAY";
+            case 6: return "JUN";
+            case 7: return "JUL";
+            case 8: return "AUG";
+            case 9: return "SEP";
+            case 10: return "OKT";
+            case 11: return "NOV";
+            case 12: return "DEC";
+
+            default: return "NULL";
+        }
     }
 
     private void Update()
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,13 +3,13 @@
     public bool FakeIdentification { get; set; }
     public string PersonIdString { get { return $"{BirthYear}{BirthMonth}{BirthDay}-{lastSixNumbers}"; } }
     public int PersonId { get; set; }
-    public string SurName { get; set; }
-    public string GivenName { get; set; }
+    public string SurName { get; set; } = "Hemmingway";
+    public string GivenName { get; set; } = "Ernst";
     public int BirthDay { get; set; }
     public int BirthMonth { get; set; }
     public int BirthYear { get; set; }
-    public int ExpireYear;
-    public int ExpireMonth;
+    public int ExpireYear { get; set; }
+    public int ExpireMonth { get; set; }
     public int lastSixNumbers { get; set; }
     public string PackageId { get; set; }
     public Sex Sex { get; set; }
